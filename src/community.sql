48a63e935ed11b034698f4dd87fb98df20fdac4c
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2023-06-30 18:50:34 -0400

    increase the tokens thing because it wasnt working

--- src/community.sql
+++ src/community.sql
@@ -126,7 +126,7 @@ CREATE TABLE `tokens` (
   `id` int(11) NOT NULL,
   `token` varchar(32) NOT NULL,
   `user` int(11) NOT NULL,
-  `ip` varchar(15) NOT NULL
+  `ip` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
